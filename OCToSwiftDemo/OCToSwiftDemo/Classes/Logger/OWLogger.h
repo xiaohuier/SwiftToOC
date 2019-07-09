3e8ec012fd622e6d927535a2194aec025478c3ae
eastzhou <eastzhou@pptv.com>
2019-07-09 22:08:17 +0800

    。。。

--- OCToSwiftDemo/OCToSwiftDemo/Classes/Logger/OWLogger.h
+++ OCToSwiftDemo/OCToSwiftDemo/Classes/Logger/OWLogger.h
@@ -6,11 +6,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#ifndef OWLoggingUtils_h
+#define OWLoggingUtils_h
 
-NS_ASSUME_NONNULL_BEGIN
+extern id sharedLogger;
+
+#define OWLogDebug(frmt, ...)         SNOPLogDebug(sharedLogger, @"PPPlayerSDK", frmt, ##__VA_ARGS__)
+#define OWLogInfo(frmt, ...)          SNOPLogInfo(sharedLogger, @"PPPlayerSDK", frmt, ##__VA_ARGS__)
+#define OWLogWarning(frmt, ...)       SNOPLogWarning(sharedLogger, @"PPPlayerSDK", frmt, ##__VA_ARGS__)
+#define OWLogError(frmt, ...)         SNOPLogError(sharedLogger, @"PPPlayerSDK", frmt, ##__VA_ARGS__)
+#endif
 
 @interface OWLogger : NSObject
 
 @end
 
-NS_ASSUME_NONNULL_END
